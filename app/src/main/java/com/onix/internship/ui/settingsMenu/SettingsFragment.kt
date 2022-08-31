9fb05740775fdf5a6b7e2fdb9bc1195fca38948a
Ognessa <vikapalihata@gmail.com>
2022-08-31 23:00:38 +0300

    Night mode

--- app/src/main/java/com/onix/internship/ui/settingsMenu/SettingsFragment.kt
+++ app/src/main/java/com/onix/internship/ui/settingsMenu/SettingsFragment.kt
@@ -1,12 +1,28 @@
 package com.onix.internship.ui.settingsMenu
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.onix.internship.R
 import com.onix.internship.arch.BaseFragment
 import com.onix.internship.databinding.FragmentSettingsBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
+
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
 
     override val viewModel: SettingsViewModel by viewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nightMode.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
 
 }
